4d6785d8c5f9510cee394e39f2d795326de50890
chen9601 <56624169+chen9601@users.noreply.github.com>
2019-11-21 20:20:41 +0900

    Add files via upload

---
+++ src/chenho/Player.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Player{
+    int power;
+    int dexterity;
+    int intelligence;
+    int health;
+    int mental;
+    TileType pos;
+    int money;
+    int status;
+    Item[] items;
+    int energy;
+}
