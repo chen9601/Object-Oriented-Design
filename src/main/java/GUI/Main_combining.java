b42ce0d2297771f685e1dc766568e489f786bd75
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2019-11-22 18:26:48 +0900

    Create Main_combining.java

---
+++ src/main/java/GUI/Main_combining.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+class MainGamePage extends JPanel{}
+class SetbossPage extends JPanel{}
+class SetStatusPage extends JPanel{}
+class StartPage extends JPanel {}
+class MainGame_statusPage extends JPanel{}
+
+public class Main_combining {
+}
