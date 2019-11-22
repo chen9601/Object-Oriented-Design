204474a1592d225c2662917d9e164ab7930fab20
chen9601 <56624169+chen9601@users.noreply.github.com>
2019-11-23 02:20:20 +0900

    수정내용
    랜덤에 난수가 int형이기 때문에 itemlist와 monsterlist 추가
    view에 있는 show_dialog_answer의 인자 answer 추가
    아 왜 구현하면 할 수록 구조가 더 이해가 안갈까..

---
+++ Item.java
@@ -0,0 +1,61 @@
+package com;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 아이템 객체 모델
+ * @author Se-Ok Jeon
+ * @version 1.0
+ */
+@Data
+@Builder
+public class Item {
+    static private Item[] items;
+    static Item[] itemlist;
+
+    enum ItemType {
+        a("엑스칼리버"),
+        b("네크로노미콘");
+
+        private String name;
+        ItemType(String name){
+            this.name = name;
+        }
+        public String getName(){
+            return name;
+        }
+    }
+// ㄴㄴ 아이템 리스트 써야함 랜덤 아이템 추가 때문에
+static                  //static itemlist 초기 설정
+{
+    itemlist[0].name="엑스칼리버";itemlist[0].p_power=2;itemlist[0].p_dex=1;itemlist[0].p_int=0;itemlist[0].p_health=1;itemlist[0].p_mental=0;
+
+    itemlist[1].name="네크로노미콘";itemlist[1].p_power=0;itemlist[1].p_dex=0;itemlist[1].p_int=4;itemlist[1].p_health=0;itemlist[1].p_mental=-1;
+
+   itemlist[2].name="빛나는 부등변다면체";itemlist[2].p_power=1;itemlist[2].p_dex=1;itemlist[2].p_int=1;itemlist[2].p_health=1;itemlist[2].p_mental=1;
+
+   itemlist[3].name="황금의 벌꿀술";itemlist[3].p_power=2;itemlist[3].p_dex=0;itemlist[3].p_int=0;itemlist[3].p_health=2;itemlist[3].p_mental=0;
+    itemlist[4].name="시간의 문제";itemlist[4].p_power=0;itemlist[4].p_dex=0;itemlist[4].p_int=2;itemlist[4].p_health=0;itemlist[4].p_mental=2;
+
+    itemlist[5].name="이븐 가지의 분말";itemlist[5].p_power=1;itemlist[5].p_dex=1;itemlist[5].p_int=1;itemlist[5].p_health=0;itemlist[5].p_mental=0;
+
+    itemlist[6].name="사냥개 부적";itemlist[6].p_power=2;itemlist[6].p_dex=2;itemlist[6].p_int=2;itemlist[6].p_health=-2;itemlist[6].p_mental=-2;
+
+  itemlist[7].name="지혜의 열쇠";itemlist[7].p_power=0;itemlist[7].p_dex=0;itemlist[7].p_int=3;itemlist[7].p_health=0;itemlist[7].p_mental=0;
+
+  itemlist[8].name="우주의 감로주";itemlist[8].p_power=1;itemlist[8].p_dex=1;itemlist[8].p_int=0;itemlist[8].p_health=2;itemlist[8].p_mental=0;
+
+itemlist[9].name="검은 파라오의 단장";itemlist[9].p_power=0;itemlist[9].p_dex=0;itemlist[9].p_int=0;itemlist[9].p_health=0;itemlist[9].p_mental=1;
+
+itemlist[10].name="알 하자드의 램프";itemlist[10].p_power=0;itemlist[10].p_dex=2;itemlist[10].p_int=1;itemlist[10].p_health=0;itemlist[10].p_mental=0;
+}
+    private int p_power;
+    private int p_dex;
+    private int p_int;
+    private int p_health;
+    private int p_mental;
+    private int price;
+    private String name;
+
+}
---
+++ Monster.java
@@ -0,0 +1,52 @@
+package com;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 몬스터의 기본 속성들을 정의하는 클래스
+ * 
+ * @author Chanho Park, Se-ok Jeon
+ */
+@Data
+@Builder
+public class Monster
+{
+
+    static private Monster[] monsters;
+    static Monster[] monsterlist;
+
+    public enum MonsterType{
+        Nightgaunt("나이트건트"),
+        Dark_Paraoh("검은 파라오"),
+        Byakhee("비야키"),
+        Ghoul("구울"),
+        Elder_Thing("옛 것");
+
+        final private String name;
+        MonsterType(String name){
+            this.name = name;
+        }
+        public String getName(){
+            return name;
+        }
+    }
+
+    static{
+        monsterlist[0].name="나이트건트";monsterlist[0].health=3;monsterlist[0].requireVal=3;monsterlist[0].damage=1;monsterlist[0].damageType=1;monsterlist[0].monsterresult=3;
+        monsterlist[1].name="검은파라오";monsterlist[1].health=1;monsterlist[1].requireVal=6;monsterlist[1].damage=2;monsterlist[1].damageType=2;monsterlist[1].monsterresult=5;
+       monsterlist[2].name="비야키";monsterlist[2].health=2;monsterlist[2].requireVal=2;monsterlist[2].damage=1;monsterlist[2].damageType=1;monsterlist[2].monsterresult=1;
+        monsterlist[3].name="구울";monsterlist[3].health=4;monsterlist[3].requireVal=4;monsterlist[3].damage=2;monsterlist[3].damageType=1;monsterlist[3].monsterresult=4;
+       monsterlist[4].name="옛 것";monsterlist[0].health=5;monsterlist[0].requireVal=4;monsterlist[0].damage=1;monsterlist[0].damageType=2;monsterlist[0].monsterresult=5;
+           }
+    private int health;
+    private int requireVal;
+    private int damage;
+    private int damageType;
+    private int monster_result;
+    private String name;
+
+    public void decreaseHealth(int damage_from_player){
+        health -= damage_from_player;
+    }
+}
