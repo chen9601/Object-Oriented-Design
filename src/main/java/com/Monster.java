68564c33caea7f30ffb1934aa86b28e5b10fd294
GoBeromsu <310o@naver.com>
2019-11-25 19:16:01 +0900

    change Monster to enum

--- src/main/java/com/Monster.java
+++ src/main/java/com/Monster.java
@@ -10,8 +10,8 @@ import lombok.Data;
  */
 @Data
 @Builder
-public class Monster
-{
+
+public class Monster {
 //    monsterlist[0].name="나이트건트";monsterlist[0].health=3;monsterlist[0].requireVal=3;monsterlist[0].damage=1;monsterlist[0].damageType=1;monsterlist[0].monsterresult=3;
 //    monsterlist[1].name="검은파라오";monsterlist[1].health=1;monsterlist[1].requireVal=6;monsterlist[1].damage=2;monsterlist[1].damageType=2;monsterlist[1].monsterresult=5;
 //    monsterlist[2].name="비야키";monsterlist[2].health=2;monsterlist[2].requireVal=2;monsterlist[2].damage=1;monsterlist[2].damageType=1;monsterlist[2].monsterresult=1;
@@ -20,29 +20,67 @@ public class Monster
 
     static private Monster[] monsters;
 
-    public enum MonsterType{
-        Nightgaunt("나이트건트"),
-        Dark_Paraoh("검은 파라오"),
-        Byakhee("비야키"),
-        Ghoul("구울"),
-        Elder_Thing("옛 것");
+    public enum MonsterType {
+        Nightgaunt("나이트건트", 3, 3, 1, 1, 3),
+        Dark_Paraoh("검은 파라오", 1, 6, 2, 2, 5),
+        Byakhee("비야키", 2, 2, 1, 1, 1),
+        Ghoul("구울", 4, 4, 2, 1, 4),
+        Elder_Thing("옛 것", 5, 4, 1, 2, 5);
 
-        final private String name;
-        MonsterType(String name){
+        private String name;
+        private int initial_health;
+        private int initial_requireVal;
+        private int initial_damage;
+        private int initial_damageType;
+        private int initial_monster_result;
+
+
+        MonsterType(String name, int health, int requireVal, int damage, int damageType, int monster_result) {
             this.name = name;
+            this.initial_health = health;
+            this.initial_requireVal = requireVal;
+            this.initial_damage = damage;
+            this.initial_damageType = damageType;
+            this.initial_monster_result = monster_result;
+
         }
-        public String getName(){
+
+
+        public String getName() {
             return name;
         }
+
+        public int getInitial_health() {
+            return initial_health;
+        }
+
+        public int getInitial_requireVal() {
+            return initial_requireVal;
+        }
+
+        public int getInitial_damage() {
+            return initial_damage;
+        }
+
+        public int getInitial_damageType() {
+            return initial_damageType;
+        }
+
+        public int getInitial_monster_result() {
+            return initial_monster_result;
+        }
     }
+
+    private String name;
     private int health;
     private int requireVal;
     private int damage;
     private int damageType;
     private int monster_result;
-    private MonsterType name;
 
-    public void decreaseHealth(int damage_from_player){
+    public void decreaseHealth(int damage_from_player) {
         health -= damage_from_player;
     }
+
+
 }
