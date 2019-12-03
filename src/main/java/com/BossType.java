8e13d037ceda6d3c6d8f58cc9c04465d799855bd
chen9601 <chpark0641@gmail.com>
2019-12-04 04:13:14 +0900

    SS

--- src/main/java/com/BossType.java
+++ src/main/java/com/BossType.java
@@ -1,21 +1,23 @@
 package com;
 
 public enum BossType {
-    AZATHOTH("아자투스",10, 10, 10,"src\\main\\java\\GUI\\imgaes\\boss\\azatoth.png"),
-    CHUTHULU("크툴루",10,3,1,"src\\main\\java\\GUI\\imgaes\\boss\\CHUTHULU.png"),
-    NYARLATHOTEP("니알라토텝",10,2,1,"src\\main\\java\\GUI\\imgaes\\boss\\NYARLATHOTEP.png");
+    AZATHOTH("아자투스",10, 10, 10,"src\\main\\java\\GUI\\imgaes\\boss\\azatoth.png","src\\main\\java\\GUI\\imgaes\\boss\\azatoth.png"),
+    CHUTHULU("크툴루",10,3,1,"src\\main\\java\\GUI\\imgaes\\boss\\CHUTHULU.png","src\\main\\java\\GUI\\imgaes\\boss\\cthulhu.gif"),
+    NYARLATHOTEP("니알라토텝",10,2,1,"src\\main\\java\\GUI\\imgaes\\boss\\NYARLATHOTEP.png","src\\main\\java\\GUI\\imgaes\\boss\\nyala.gif");
 
     final private String name;
     private int damage;
     private int health;
     private int requireVal;
     private String boss_imgpath;
-    BossType(String name, int health, int damage, int requireVal,String boss_imgpath){
+    private String boss_activepath;
+    BossType(String name, int health, int damage, int requireVal,String boss_imgpath,String boss_activepath){
         this.name = name;
         this.health=health;
         this.damage=damage;
         this.requireVal=requireVal;
         this.boss_imgpath = boss_imgpath;
+        this.boss_activepath=boss_activepath;
     }
     public String getName(){
         return name;
@@ -29,6 +31,7 @@ public enum BossType {
     public int getRequireVal(){
         return requireVal;
     }
+    public String getActive_path(){return boss_activepath;}
 
     public String getBoss_imgpath(){return boss_imgpath;}
 }
