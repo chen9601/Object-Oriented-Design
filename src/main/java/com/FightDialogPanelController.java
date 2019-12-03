117c6b4638041f1181378f9664098653aa22f026
chen9601 <chpark0641@gmail.com>
2019-12-03 18:12:05 +0900

    ss

--- src/main/java/com/FightDialogPanelController.java
+++ src/main/java/com/FightDialogPanelController.java
@@ -34,7 +34,7 @@ public class FightDialogPanelController extends DialogPanelController{
 
         StringBuilder message=new StringBuilder();
         message.append(monster.getMonster_type().getName());
-        message.append("와 마주쳤다!");
+        message.append("와(과) 마주쳤다!");
         show_dialog(message.toString());
         show_dialog_answer1(new Answer("몬스터를 공격한다.", "attack"));
         show_dialog_answer2(new Answer("도망친다.","run"));
@@ -70,11 +70,27 @@ public class FightDialogPanelController extends DialogPanelController{
                 RandomEventAnswer.AnswerIndicator(Player.getCurrentPlayer(), answer);
                 selectbtn1.revalidate();
                 selectbtn1.repaint();
+                if(Player.getCurrentPlayer().getHealth()<1||FightDialogPanelController.monster.getHealth()<1)
+                {
+                    FightMonsterController.fight_monster_page.dispose();
+                }
             }
         });
         selectbtn1.setVisible(true);
     }
 
+    public static void show_dialog_answer2(Answer answer){
+        selectbtn2.setText(answer.getMessage());
+        selectbtn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RandomEventAnswer.AnswerIndicator(Player.getCurrentPlayer(), answer);
+                selectbtn2.revalidate();
+                selectbtn2.repaint();
+            }
+        });
+        selectbtn2.setVisible(true);
+    }
     public static void getplayerAttackDialog(){
 
     }
