3d02e3c39ef85f12cf4a9cfc16ca368462ffae80
chen9601 <chpark0641@gmail.com>
2019-12-05 21:20:56 +0900

    javadoc 추가

--- src/main/java/com/ConstantEventHandler.java
+++ src/main/java/com/ConstantEventHandler.java
@@ -3,6 +3,7 @@ package com;
 import GUI.Dice_page;
 import GUI.Fight_monster_page;
 
+import static com.GameMaster.isTherePlayer;
 import static com.Map.*;
 
 import java.io.IOException;
@@ -24,11 +25,25 @@ public class ConstantEventHandler
     static boolean shop_maintain_switch=false;
     static int fight_remain=0;
 
+    /**
+     * 랜덤 이벤트를 발생시키는 메소드
+     * @param player
+     * 랜덤 이벤트를 진행할 플레이어
+     */
     static void generateRandomEvent(Player player)
     {
         int event_num=(int)Math.floor(Math.random()*21);
         RandomEventHandler.RandomEventIndicator(player, event_num);
     }
+
+    /**
+     * 맵을 클릭하여 플레이어가 이동하려고 할 때 이동시키는 메소드
+     * @param player
+     * 해당 플레이어
+     * @param tile
+     * 목적지
+     * @throws IOException
+     */
     static public void move(Player player, TileType tile) throws IOException {
         boolean valid = pathValid(player, tile);
 
@@ -38,7 +53,19 @@ public class ConstantEventHandler
             CheckEventHere(player, tile);
         }
     }
-    static void movebyTeleport(Player player, TileType tile){player.setPos(tile);}
+
+    /**
+     * 이벤트로 인해 플레이어가 이동해야 할 때 이동시키는 메소드
+     * <br>이 이벤트로 인해 이벤트가 발생해야하는 곳으로 갈 경우에는 이벤트가 발생하지 않는다.
+     * @param player
+     * 해당 플레이어
+     * @param tile
+     * 목적지
+     * @throws IOException
+     */
+    static void movebyTeleport(Player player, TileType tile) throws IOException {
+        player.setPos(tile);
+    }
     /**
      * 플레이어가 움직인 이후 호출, 움직일 플레이어와 목적지를 받아 이벤트 여부를 확인하는 메소드
      * @param player
@@ -83,6 +110,14 @@ public class ConstantEventHandler
         }
 
     }
+
+    /**
+     * 플레이어에게 아이템을 추가하고 그에 대한 능력 추가치를 적용시키는 메소드
+     * @param player
+     * 해당 플레이어
+     * @param item
+     * 얻게 되는 아이템
+     */
     static void addItem(Player player, ItemType item)
     {
         ArrayList<ItemType> tempitems = player.getItems();
@@ -94,6 +129,12 @@ public class ConstantEventHandler
         player.setDexterity(player.getDexterity()+item.getDex());
         player.setIntelligence(player.getIntelligence()+item.getInt());
     }
+
+    /**
+     * 플레이어에게 무작위 아이템을 추가시키고 능력 추가치를 적용하는 메소드
+     * @param player
+     * 해당 플레이어
+     */
     static void addRandomItem(Player player)
     {
         int tempRand=(int)Math.floor(Math.random()*15);
@@ -108,26 +149,46 @@ public class ConstantEventHandler
     }
 
 
+    /**
+     * 병원 이벤트에 관한 메소드
+     * @param player
+     * 병원에 도착한 플레이어
+     */
     static void hospital(Player player)
     {
         String message="병원에 도착했습니다. 치료받으시겠습니까?";
         DialogPanelController.show_dialog(message);
-        DialogPanelController.show_dialog_answer1(ConstantEventAnswer.hospital1);
-        DialogPanelController.show_dialog_answer2(ConstantEventAnswer.hospital2);
+        DialogPanelController.show_dialog_answer1(new Answer("1. 치료한다","hospital1"));
+        DialogPanelController.show_dialog_answer2(new Answer("2. 떠난다","hospital2"));
     }
-
+    /**
+     * 정신병원 이벤트에 관한 메소드
+     * @param player
+     * 정신병원에 도착한 플레이어
+     */
     static void mospital(Player player)
     {
         String message="정신병원에 도착했습니다. 치료받으시겠습니까?";
         DialogPanelController.show_dialog(message);
-        DialogPanelController.show_dialog_answer1(ConstantEventAnswer.mospital1);
-        DialogPanelController.show_dialog_answer2(ConstantEventAnswer.mospital2);
+        DialogPanelController.show_dialog_answer1(new Answer("1. 치료한다","mospital1"));
+        DialogPanelController.show_dialog_answer2(new Answer("2. 떠난다","mospital2"));
     }
 
+    /**
+     * 일반 몬스터 전투 창으로 넘어가게 하는 메소드
+     * @param monster
+     * 전투할 몬스터
+     * @throws IOException
+     */
     static void generateFight(MonsterType monster) throws IOException {
         Fight_monster_page fightwithmonster = new Fight_monster_page(new Monster(monster));
     }
 
+    /**
+     * 주사위 창을 출력하는 메소드
+     * @return Dice.getSavedDice_num()
+     * 1~6 중에 무작위로 선택된 int형의 값
+     */
     public static int  Dice(){
         Dice_page Dice = new Dice_page();
         Dice.setVisible(true);
@@ -142,6 +203,14 @@ public class ConstantEventHandler
         return Dice.getSavedDice_num();
     }
 
+    /**
+     * 해당 아이템이 플레이어가 소지한 금액으로 구매할 수 있는지 확인하는 메소드
+     * @param player
+     * 플레이어
+     * @param item
+     * 구매하려고 하는 아이템
+     * @return 구매 가능 여부
+     */
     static boolean isAffordable(Player player, ItemType item)
     {
         if(player.getMoney()<item.getPrice())
@@ -149,11 +218,24 @@ public class ConstantEventHandler
         else
             return true;
     }
+
+    /**
+     * 아이템 구매 시 구매 후 잔액을 플레이어에게 넘겨주는 메소드
+     * @param player
+     * 플레이어
+     * @param item
+     * 구매한 아이템
+     * @return 구매 후 잔액
+     */
     static int getChange(Player player, ItemType item)
     {
         return player.getMoney()-item.getPrice();
     }
 
+    /**
+     * 상점에 도착할 때마다 아이템 리스트를 갱신하는 메소드
+     * @return 무작위로 선정된 아이템 리스트
+     */
     static ArrayList<ItemType> getRandomItemList()
     {
         ArrayList<ItemType> tempitemlist = new ArrayList<ItemType>();
@@ -175,15 +257,31 @@ public class ConstantEventHandler
         return tempitemlist;
     }
 
+    /**
+     * 몬스터를 생성하는 메소드
+     * <br>몬스터는 병원, 상점, 정신병원, 플레이어가 위치하고 있는 곳, 포털이나 몬스터가 이미 존재하는 곳에는 생성될 수 없다.
+     * @param monster
+     * 생성될 몬스터
+     * @param tile
+     * 몬스터가 생성될 장소
+     */
     static void CreateMonster(MonsterType monster, TileType tile)
     {
-        if(Map.tiles[tile.ordinal()].getSummoned_monster()==null&&Map.tiles[tile.ordinal()].isSummoned_portal()==false)
+        if(isTherePlayer(Map.tiles[tile.ordinal()])
+                &&Map.tiles[tile.ordinal()].getSummoned_monster() == null
+                &&Map.tiles[tile.ordinal()].isSummoned_portal()==false
+                &&Map.tiles[tile.ordinal()].getTile_type()!=TileType.MENTAL_HOSPITAL
+                &&Map.tiles[tile.ordinal()].getTile_type()!=TileType.HOSPITAL
+                &&Map.tiles[tile.ordinal()].getTile_type()!=TileType.STORE)
         {
             Monster tempMon=new Monster(monster);
             Map.tiles[tile.ordinal()].setSummoned_monster(tempMon);
         }
     }
 
+    /**
+     * 상점 이벤트를 진행시키는 메소드
+     */
     static void shop()
     {
         String message="상점에 도착했습니다. 구매하실게 있으신가요?";
